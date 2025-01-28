bd0382907082c7509816b5694721579f39e760b9
TaiAop <thayaop@gmail.com>
2025-01-28 15:26:37 +0000

    ALTERAÇÃO DO CÓD PARA REGISTRO DE CARTAS - NOVATO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,9 +4,44 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Tainara
 
 int main() {
+
+char country[1];
+int state;
+
+int Pop;
+float PIB;
+int Tur;
+float area;
+
+    printf("PAÍS: \n");
+    scanf("%s", &country);
+
+    printf("ESTADO: \n");
+    scanf("%d", &state);
+
+    printf("População: \n");
+    scanf("%d", &Pop);
+
+    printf("PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Área: \n");
+    scanf("%f", &area);
+
+    printf("N° de Pontos Turísticos: \n");
+    scanf("%d", &Tur);
+
+printf("CARTA: %s %d\n", country, state);
+printf("População: %d\n - PIB: %f\n - Área: %f\n - N° de Pontos Turísticos: %d\n", Pop, PIB, area, Tur);
+
+
+return 0;
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -18,5 +53,5 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+
 }
